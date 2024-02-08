2a1270fd89d76dab06bd36d11c09fe1f2da3b700
SerhiiBorozenets <serhiiborozenets@gmail.com>
2024-02-08 15:13:19 +0000

    add bootstrap

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
   return (
-    <div>
+    <div className='mt-3 p-3 text-primary'>
       <p>You clicked {count} times!</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
